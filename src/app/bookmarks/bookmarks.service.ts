2a4b2e2b8cd9a7a7528777957cbbff8d5dae3cfe
Anna <mrowicka.ania@gmail.com>
2019-11-21 15:24:59 +0100

    json server

--- src/app/bookmarks/bookmarks.service.ts
+++ src/app/bookmarks/bookmarks.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core'
 import { Bookmark, BookmarkId } from './bookmarks.models'
 import { BehaviorSubject } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { log } from 'util'
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
@@ -10,21 +13,30 @@ export class BookmarksService {
   // tslint:disable-next-line:variable-name
   private _items$ = new BehaviorSubject<Bookmark[]>([])
   items$ = this._items$.asObservable()
+  private readonly url = 'http://localhost:3000/bookmarks'
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).subscribe(bookmarks => this.update)
+  }
 
   add(item: Bookmark): void {
-    this.update([...this.items, item])
+    this.http
+      .post(this.url, item)
+      .pipe(map(() => [...this.items, item]))
+      .subscribe(this.update)
   }
 
   remove(id: BookmarkId): void {
-    this.update(this.items.filter(item => item.id !== id))
+    this.http
+      .delete(`${this.url}/${id}`)
+      .pipe(map(() => this.items.filter(item => item.id !== id)))
+      .subscribe(this.update)
   }
 
   has(id: BookmarkId): boolean {
     return this.items.some(item => item.id === id)
   }
-  private update(item: Bookmark[]) {
+  private update = (item: Bookmark[]) => {
     this.items = item
     this._items$.next(item)
   }
