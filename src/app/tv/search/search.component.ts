5763062085909c407b65bb482a3347c1a2242f7a
Anna <mrowicka.ania@gmail.com>
2019-11-22 09:44:41 +0100

    reactive forms part 2

--- src/app/tv/search/search.component.ts
+++ src/app/tv/search/search.component.ts
@@ -3,7 +3,9 @@ import { HttpClient } from '@angular/common/http'
 import { Show } from '../tv.models'
 import { TvMazeService } from '../tv-maze.service'
 import { BookmarksService } from '../../bookmarks/bookmarks.service'
-import { FormBuilder, FormGroup } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { debounceTime, distinctUntilChanged, filter, map, tap } from 'rxjs/operators'
+import { log } from 'util'
 
 @Component({
   selector: 'tm-search',
@@ -23,14 +25,27 @@ export class SearchComponent {
     private fb: FormBuilder,
   ) {
     this.search('batman')
-    const query = this.fb.control('superman')
+    const query = this.fb.control('superman', [Validators.required, Validators.minLength(3)])
     this.searchForm = this.fb.group({
       query,
     })
-    this.searchForm.valueChanges.subscribe(v => v.query)
+    // this.searchForm.valueChanges.subscribe(v => v.query);
+    // this.searchForm.controls.query.value
+    // this.searchForm.get('query')
+    // this.searchForm.get(['adress', 4]);
+    this.searchForm.valueChanges
+      .pipe(
+        map(v => v.query),
+        tap(v => console.log(this.searchForm.get('query').errors)),
+        filter(() => this.searchForm.valid),
+        debounceTime(200),
+        distinctUntilChanged(),
+      )
+      .subscribe(this.search)
   }
 
-  search(query: string) {
+  search = (query: string) => {
+    console.log(this)
     this.tv.searchShows(query).subscribe(shows => (this.shows = shows))
   }
 }
